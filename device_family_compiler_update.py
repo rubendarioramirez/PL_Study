bf8b6b13a8a1c8b115cbe707bd9b8b46241cd9c4
Ruben Ramirez <ramirez.ruben.dario10@gmail.com>
2018-06-11 14:20:46 +0700

    +Fixed CPU parsing

--- device_family_compiler_update.py
+++ device_family_compiler_update.py
@@ -22,14 +22,12 @@ gpu_counterparts = pd.read_csv(gpu_file)
 #Create a GPU dictonary
 gpu_dict = gpu_counterparts.set_index('GPU').to_dict()
 
-
 ######## Initial Mungling ################################################
 
 ##Fill with np.nan for non existing values
 df_raw = df_raw.replace(r'^\s+$', np.nan, regex=True)
 df_raw['Counterpart'].replace(r'^-', np.nan, regex=True, inplace = True)
 
-##Verifying Adreno 530
 df_raw[(df_raw['SOC']=='Qualcomm MSM8996') | (df_raw['SOC']=='Qualcomm APQ8096')]['GPU'] = 'Adreno 530'
 ##Verifying Adreno 510
 df_raw[(df_raw['SOC']=='Qualcomm MSM8994') | (df_raw['SOC']=='Qualcomm APQ8094')]['GPU'] = 'Adreno 430'
@@ -42,15 +40,13 @@ df_counterpart = df_raw[df_raw['Counterpart'].isnull() == False]
 
 #Now map it like in the cowboy movies        
 df_nocounter['Counterpart'] = df_nocounter.GPU.map(gpu_dict)
-#Concat to a new DF with more devices
-df = pd.concat([df_counterpart, df_nocounter])
+
 
 #Create a dictonary of GPUS and Counterparts
 gpu_dict={}
 for x in df_counterpart['GPU'].unique():
     if len(df_counterpart[(df_counterpart['GPU'] == str(x))]['Counterpart'].unique()) > 0:
         gpu_dict[x]=str(df_counterpart[(df_counterpart['GPU'] == str(x))]['Counterpart'].unique()[0])
-
 gpu_dict['Qualcomm Adreno 509 (370Mhz)'] = 'iPhone 5s'
 gpu_dict['Intel HD Graphics 500 (650Mhz)'] ='iPhone 6-'
 gpu_dict['Intel HD Graphics 400 (600Mhz)'] ='iPhone 5s+'
@@ -59,9 +55,33 @@ gpu_dict['ARM Mali T820 (550Mhz)'] ='iPhone 5s-'
 gpu_dict['3x ARM Mali G72 (800Mhz)'] ='iPhone 8'
 gpu_dict['Imagination Tech PowerVR GE8320 (650Mhz)'] ='iPhone 5s+'
 
-df['Counterpart'] = df['GPU'].map(gpu_dict)
+#Map by GPU with counterpart    
+df_nocounter['Counterpart'] = df_nocounter.GPU.map(gpu_dict)
 
-df.isnull().sum()
+
+#Create a dictonary of SOC and Counterparts
+soc_dict={}
+for x in df_counterpart['SOC'].unique():
+    if len(df_counterpart[(df_counterpart['SOC'] == str(x))]['Counterpart'].unique()) > 0:
+        soc_dict[x]=str(df_counterpart[(df_counterpart['SOC'] == str(x))]['Counterpart'].unique()[0])
+#Map by SOC with counterpart      
+df_nocounter['Counterpart'] = df_nocounter.SOC.map(soc_dict)
+
+#Concat to a new DF with more devices
+df = pd.concat([df_counterpart, df_nocounter])
+
+##Adding 3x ARM Mali G72 (800Mhz)
+armg72 = df['GPU']=='3x ARM Mali G72 (800Mhz)'
+df['Counterpart'][armg72] = 'iPhone 8'
+adreno509 = df['GPU']=='Qualcomm Adreno 509 (370Mhz)'
+df['Counterpart'][adreno509] = 'iPhone 5s'
+intel400 = df['GPU']=='Intel HD Graphics 400 (600Mhz)'
+df['Counterpart'][intel400] = 'iPhone 5s+'
+imagine = df['GPU']=='Imagination Tech PowerVR GE8100 (570Mhz)'
+df['Counterpart'][imagine] = 'iPhone 5-'
+
+#df.Counterpart.unique()
+#df.isnull().sum()
 
 #### RAM PARSING #####################################
 
@@ -91,7 +111,7 @@ df['RAM_FAM'] = df['RAM'].apply(add_ram_fam)
 ##Create a list of GPUs per type, LOW, MID, HIGH
 low_gpu_list = ['Below iPhone 4s','iPhone 4s','iPhone 4s-','iPhone 4s+','iPhone 5','iPhone 5-','iPhone 5+']
 mid_gpu_list = ['iPhone 5s','iPhone 5s-','iPhone 5s+','iPhone 6','iPhone 6-','iPhone 6+']
-high_gpu_list = ['iphone 6s','iPhone 6s-','iPhone 6s+','iPhone 7','iPhone 7-','iPhone 7+','iPhone 8+','iPhone 8-','iPhone 8+']
+high_gpu_list = ['iPhone 6s','iPhone 6s-','iPhone 6s+','iPhone 7','iPhone 7-','iPhone 7+','iPhone 8','iPhone 8+','iPhone 8-','iPhone 8+']
 
 df.Counterpart.unique()
 ##This function will check for each row if the GPU is in the list above, if it's there 
@@ -109,6 +129,8 @@ def add_gpu_fam(row):
         return result
     
 df['GPU_FAM'] = df['Counterpart'].apply(add_gpu_fam)
+
+df.isnull().sum()
 
 ##### CPU PARSING #################################
 
@@ -143,13 +165,15 @@ def add_cpu_fam(row):
                 #The other one is also 2X
                 if bestCPU == 0:
                     return 'LOW-CPU'
+                elif freq > 2000:
+                    return 'MID-CPU'
                 #The second is bigger but lower frequency
                 elif int(b) > 2:
                     if freq < 1800:
                         return 'LOW-CPU'
                     else:
                         return 'MID-CPU'
-            elif int(a) == 4: 
+            if int(a) == 4: 
                 if freq > 1800 and freq < 2400:
                     return 'MID-CPU'
                     #if it has big freq
@@ -158,11 +182,12 @@ def add_cpu_fam(row):
                 elif freq <= 1800:
                     return 'LOW-CPU'
             #It's bigger than 4X
-            elif int(a) == 6:
+            if int(a) == 6:
                 return 'MID-CPU'
-            elif int(a) == 8:
+            if int(a) == 8:
                 return 'MID-CPU'
             
+                        
     #If just 1 CPU
     elif len(str(row).split("; ")) == 1:
         a = str(row).split("; ")[0][0] 
@@ -187,6 +212,20 @@ def add_cpu_fam(row):
 
 df['CPU_FAM'] = df['CPU'].apply(add_cpu_fam)
 
+
+
+faulty = df['CPU'] == '4x ARM Cortex-A9 (1400Mhz); ARM Cortex-A9 (1500Mhz)'
+faulty2 = df['CPU'] == '4x ARM Cortex-A9 (1600Mhz); ARM Cortex-A9 (1700Mhz)' 
+faulty3 = df['CPU'] == '4x ARM Cortex-A15 (1900Mhz); ARM Cortex-A15 (1900Mhz)' 
+faulty4 = df['CPU'] == '4x ARM Cortex-A9 (1200Mhz); ARM Cortex-A9 (1300Mhz)' 
+faulty5 = df['CPU'] == '4x ARM Cortex-A15 (2300Mhz); ARM Cortex-A15 (2300Mhz)' 
+df['CPU_FAM'][faulty] = 'LOW-CPU'
+df['CPU_FAM'][faulty2] = 'LOW-CPU'
+df['CPU_FAM'][faulty3] = 'MID-CPU'
+df['CPU_FAM'][faulty4] = 'LOW-CPU'
+df['CPU_FAM'][faulty5] = 'MID-CPU'
+
+df.isnull().sum()
 ####################### FIND POSSIBLE FAMILIES #######################################
 
 cpu_list = list(df['CPU_FAM'].unique())
@@ -199,11 +238,6 @@ ram_list = list(df['RAM_FAM'].unique())
 ram_list = ram_list[:3]
 
 possible_fam = list(itertools.product(cpu_list,gpu_list,ram_list))
-
-#with open('family_list.csv', "w") as output:
-#    writer = csv.writer(output, lineterminator='\n')
-#    for val in possible_fam:
-#        writer.writerow([val])
 
 #Create a new cell with possible families to match later.        
 df['merged_cells'] = df['CPU_FAM'] + ", " + df['GPU_FAM'] + ", " + df['RAM_FAM']
@@ -249,7 +283,7 @@ df.aspect_ratio.unique()
 
 #################### OUPUT THE NEW FILE ##################
     
-writer = pd.ExcelWriter('pl_9june.xlsx')
+writer = pd.ExcelWriter('pl_11june.xlsx')
 df.to_excel(writer,'total')
 writer.save()
 
